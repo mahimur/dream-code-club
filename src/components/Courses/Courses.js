df80b4a89d726e659bb7f694ad21a72a545a33be
mahimur <hellomahimur@gmail.com>
2022-10-10 20:46:59 +0600

    first commit

---
+++ src/components/Courses/Courses.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Courses.css'
+
+const Courses = () => {
+    return (
+        <div className='container'>
+            <div className='exercise-container'>
+                <div>
+                    <h1>Dream-Coding-Club</h1>
+                    <h3>Select today's exercise</h3>
+                </div>
+                <div className='exercises'>
+
+                </div>
+            </div>
+            <div className='exercise-details'>
+                <h1>thias is cart</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Courses;
